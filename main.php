f70851023d59a8197fd672b9c9774ed048291072
Edinyanga O. Ottoho <elzucky@gmail.com>
2021-08-03 00:26:29 +0100

    Update main.php

--- main.php
+++ main.php
@@ -7,23 +7,23 @@ function projectedEarnings($data, $days) {
 		$total_capital += $i["amount"];
 		foreach ($data as $v=>$j) {
 			if ($k != $v) {
-			    array_push($timestamps, abs($j["timestamp"] - $i["timestamp"]));//difference in seconds across data items
-			    array_push($amounts, $i["amount"]);//amounts
+			    array_push($timestamps, abs($j["timestamp"] - $i["timestamp"])); //difference in seconds across data items
+			    array_push($amounts, $i["amount"]); //amounts
 			}
 		}
 	}
 	$ef_amounts = count($amounts);
 	$ef_timestamps = count($timestamps);
 	if ($ef_amounts == 0 || $ef_timestamps == 0) {
-                //prevents DivisionByZero error
-		return 0;
+        //prevents DivisionByZero error
+		return $total_capital;
 	}
 	else {
 		$average_numerator = array_sum($amounts) / count($amounts);
-		$average_denominator = (array_sum($timestamps) / count($timestamps))/86400;//convert from amount/sec to amount/day
+		$average_denominator = (array_sum($timestamps) / count($timestamps))/86400; //convert from amount/sec to amount/day
 		$rate_per_day = ($average_numerator / $average_denominator);
 		function arithmetic_progression($a, $n, $d) {
-			return $a + ($n-1) * $d;//nth term of a series (AP)
+			return $a + ($n-1) * $d; //nth term of a series (AP)
 		}
 		$ap = arithmetic_progression($total_capital, $days, $rate_per_day);
 		return round($ap);
